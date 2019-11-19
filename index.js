4657e903af730611c5614d974335a9b6111a3989
Hrithik Naha <naha.hrithkk@gmail.com>
2019-11-19 14:23:39 +0530

    Add sync chain on connect code

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const PubSub = require('./pubsub-pubnub');
@@ -6,6 +7,9 @@ const PubSub = require('./pubsub-pubnub');
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 setTimeout(() => pubsub.broadcastChain(), 1000);
 
@@ -25,7 +29,20 @@ app.post('/api/mine', (req, res) => {
 	res.redirect('/api/blocks');
 });
 
-const DEFAULT_PORT = 3000;
+const synChains = () => {
+	request(
+		{ url: `${ROOT_NODE_ADDRESS}/api/blocks` },
+		(error, response, body) => {
+			if (!error && response.statusCode === 200) {
+				const rootChain = JSON.parse(body);
+
+				console.log('replace chain in sync with', rootChain);
+				blockchain.replaceChain(rootChain);
+			}
+		}
+	);
+};
+
 let PEER_PORT;
 
 if (process.env.GENERATE_PEER_PORT === 'true') {
@@ -35,4 +52,6 @@ if (process.env.GENERATE_PEER_PORT === 'true') {
 const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
 	console.log(`Application is listening on localhost:${PORT}`);
+
+	synChains();
 });
