e26e0ece15acef7b97e8a6af9d4e62439437b582
Hrithik Naha <naha.hrithkk@gmail.com>
2019-11-19 15:39:30 +0530

    Add verification of data

--- util/index.js
+++ util/index.js
@@ -1,5 +1,12 @@
 const EC = require('elliptic').ec;
+const cryptoHash = require('./crypto-hash');
 
 const ec = new EC('secp256k1'); //standards of efficient cyptography prime 256 bits koblits first implementation
 
-module.exports = { ec };
+const verifySignature = ({ publicKey, data, signature }) => {
+	const keyFromPublic = ec.keyFromPublic(publicKey, 'hex');
+
+	return keyFromPublic.verify(cryptoHash(data), signature);
+};
+
+module.exports = { ec, verifySignature };
