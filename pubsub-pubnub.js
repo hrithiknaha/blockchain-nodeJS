7936215d3e36c7af13603970e92b63964d25df29
Hrithik Naha <naha.hrithkk@gmail.com>
2019-11-19 13:26:44 +0530

    Add broadcast chain to pubnub

--- pubsub-pubnub.js
+++ pubsub-pubnub.js
@@ -7,11 +7,14 @@ const credentials = {
 };
 
 const CHANNELS = {
-	TEST: 'TEST'
+	TEST: 'TEST',
+	BLOCKCHAIN: 'BLOCKCHAIN'
 };
 
 class PubSub {
-	constructor() {
+	constructor({ blockchain }) {
+		this.blockchain = blockchain;
+
 		this.pubnub = new PubNub(credentials);
 
 		this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
@@ -27,12 +30,25 @@ class PubSub {
 				console.log(
 					`Message recieved, Channel:${channel}. Message:${message}.`
 				);
+
+				const parsedMessage = JSON.parse(message);
+
+				if (channel == CHANNELS.BLOCKCHAIN) {
+					this.blockchain.replaceChain(parsedMessage);
+				}
 			}
 		};
 	}
 
 	publish({ channel, message }) {
 		this.pubnub.publish({ channel, message });
+	}
+
+	broadcastChain() {
+		this.publish({
+			channel: CHANNELS.BLOCKCHAIN,
+			message: JSON.stringify(this.blockchain.chain)
+		});
 	}
 }
 
