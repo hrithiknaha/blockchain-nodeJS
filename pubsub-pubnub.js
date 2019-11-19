b81fc08ca16175fb8760e7654f07ddca15c55aba
Hrithik Naha <naha.hrithkk@gmail.com>
2019-11-19 14:41:55 +0530

    removed redundancy pubnub

--- pubsub-pubnub.js
+++ pubsub-pubnub.js
@@ -40,8 +40,17 @@ class PubSub {
 		};
 	}
 
+	// publish({ channel, message }) {
+	// 	this.pubnub.publish({ channel, message });
+	// }
+
 	publish({ channel, message }) {
-		this.pubnub.publish({ channel, message });
+		this.pubnub.unsubscribe({ channel });
+		setTimeout(() => this.pubnub.publish({ channel, message }), 3000);
+		setTimeout(
+			() => this.pubnub.subscribe({ channels: [Object.values(CHANNELS)] }),
+			6000
+		);
 	}
 
 	broadcastChain() {
