2a14173b99d6fd6830c7a3f6fb6e13c5181b5bf3
Hrithik Naha <naha.hrithkk@gmail.com>
2019-11-22 21:37:04 +0530

    Add api /api/transact

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const request = require('request');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain/blockchain');
 const PubSub = require('./app/pubsub-pubnub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
 
 const app = express();
 const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain });
 
 const DEFAULT_PORT = 3000;
@@ -25,6 +29,15 @@ app.post('/api/mine', (req, res) => {
 	pubsub.broadcastChain();
 
 	res.redirect('/api/blocks');
+});
+
+app.post('/api/transact', (req, res) => {
+	const { amount, recipient } = req.body;
+	const transaction = wallet.createTransaction({ recipient, amount });
+
+	transactionPool.setTransaction(transaction);
+
+	res.json({ transaction });
 });
 
 const synChains = () => {
