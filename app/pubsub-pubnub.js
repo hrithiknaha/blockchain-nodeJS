e4409c8e1dd5e388d8307d0a11af7ccebf054fbf
Hrithik Naha <naha.hrithkk@gmail.com>
2019-11-25 00:16:29 +0530

    transaction.update fix

--- app/pubsub-pubnub.js
+++ app/pubsub-pubnub.js
@@ -13,9 +13,10 @@ const CHANNELS = {
 };
 
 class PubSub {
-	constructor({ blockchain, transactionPool }) {
+	constructor({ blockchain, transactionPool, wallet }) {
 		this.blockchain = blockchain;
 		this.transactionPool = transactionPool;
+		this.wallet = wallet;
 
 		this.pubnub = new PubNub(credentials);
 
@@ -40,7 +41,13 @@ class PubSub {
 						this.blockchain.replaceChain(parsedMessage);
 						break;
 					case CHANNELS.TRANSACTION:
-						this.transactionPool.setTransaction(parsedMessage);
+						if (
+							!this.transactionPool.existingTransaction({
+								inputAddress: this.wallet.publicKey
+							})
+						) {
+							this.transactionPool.setTransaction(parsedMessage);
+						}
 						break;
 					default:
 						return;
